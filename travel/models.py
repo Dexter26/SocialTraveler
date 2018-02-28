0036bad92870196f0d0b74fc9beaa23fd0fea745
vp60132n <vp60132n@pace.edu>
2018-02-28 16:03:47 -0500

    Changed Neo4J connection to new cloud GrapheneDB hosting remote location

--- travel/models.py
+++ travel/models.py
@@ -3,7 +3,7 @@ from __future__ import unicode_literals
 from django.db import models
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from py2neo import Graph
+from py2neo import Graph,authenticate
 
 
 # Create your models here.
@@ -18,22 +18,25 @@ class Todo(models.Model):
 '''
     Only called when Post Save method executes based on the @receiver signal
 '''
-# @receiver(post_save, sender=Todo)
-# def create_todo_node(sender, instance, **kwargs):
-#     """
-#     Creates the todo node on the graph database.
-#     """
-#   
-#     if instance is not None:
-#         #graph = Graph('http://localhost:7474/db/data')
-#         graph = Graph("http://neo4j:password@localhost:7474")
-#         query = '''
-#         CREATE (n:Todo { title : {title}, description : {description}})
-#         '''
-#   
-#         ## now execute the query
-#         graph.run(query, title=instance.title,
-#                 description=instance.description
-#         )
-#          
+@receiver(post_save, sender=Todo)
+def create_todo_node(sender, instance, **kwargs):
+    """
+    Creates the todo node on the graph database.
+    """
+   
+    if instance is not None:
+        #graph = Graph('http://localhost:7474/db/data')
+        #graph = Graph("http://neo4j:password@localhost:7474")
+        authenticate("hobby-hnhnphgpkdehgbkecgpgnpal.dbs.graphenedb.com:24786", "traveldiaries", "b.5yAEyKlxfCO5.VNM1FNl0f9b0BjaP")
+        graph = Graph("bolt://hobby-hnhnphgpkdehgbkecgpgnpal.dbs.graphenedb.com:24786", user="traveldiaries", password="b.5yAEyKlxfCO5.VNM1FNl0f9b0BjaP", bolt=True, secure = True, http_port = 24789, https_port = 24780)
+        #graph = Graph("https://traveldiaries:b.5yAEyKlxfCO5.VNM1FNl0f9b0BjaP@hobby-hnhnphgpkdehgbkecgpgnpal.dbs.graphenedb.com:24786")
+        query = '''
+        CREATE (n:Todo { title : {title}, description : {description}})
+        '''
+   
+        ## now execute the query
+        graph.run(query, title=instance.title,
+                description=instance.description
+        )
+          
 
